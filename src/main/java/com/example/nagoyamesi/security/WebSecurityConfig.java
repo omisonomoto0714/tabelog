93a03dd27a1be4b5b2ce04fff01da55537fb99cf
omisonomoto0714 <omisonomoto0618@gmail.com>
2024-06-19 22:35:03 +0900

    ログイン画面、認証クラスまで

---
+++ src/main/java/com/example/nagoyamesi/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.nagoyamesi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+		.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/").permitAll() //すべてのユーザー許可
+				.requestMatchers("/admin/**").hasRole("ADMIN")  //管理者のみ
+				.anyRequest().authenticated()  //それ以外はログインが必須
+				)
+				.formLogin((form) -> form
+						.loginPage("/login")  //ログインページ
+						.loginProcessingUrl("/login")  //ログインフォームの送信先
+						.defaultSuccessUrl("/?loggedIn")  //ログイン成功時の送信先
+						.failureUrl("/login?error")  //ログイン失敗時の送信先
+						.permitAll()
+						)
+				.logout((logout) -> logout
+						.logoutSuccessUrl("/?loggedOut")  //ログアウト後のリダイレクト先
+						.permitAll()
+						);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
